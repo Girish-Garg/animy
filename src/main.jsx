e6db22f748da6103fbe59c9da25ea04e4d5e8834
Girish Garg <garggirish2020@gmail.com>
2025-07-24 14:11:14 +0530

    deploying updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
@@ -14,5 +16,7 @@ createRoot(document.getElementById('root')).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={'/'}>
       <App />
     </ClerkProvider>
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
